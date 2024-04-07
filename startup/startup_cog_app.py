4a1d1382d28ff50c2fbc91dc991eb11117d21b66
f4teee <yqzhang1122@gmail.com>
2024-04-07 18:11:05 +0800

    feat: update

--- startup/startup_cog_app.py
+++ startup/startup_cog_app.py
@@ -10,15 +10,8 @@ Copyright (c) 2024 by zyq, All Rights Reserved.
 
 import uvicorn
 
-# from app.qilin_app import MedVlmApp
 from app.cogagent_app import CogVLMApp
 from configs.server_config import get_server_config
-
-# def create_med_app(model_name: str = 'qilin_med_vlm'):
-#     model_server_config = get_server_config(model_name)
-#     model_path = model_server_config['model_path']
-#     my_app = MedVlmApp(model_path).create_medvlm_app()
-#     uvicorn.run(my_app, host=model_server_config['host'], port=model_server_config['port'])
 
 def create_cog_app(model_name: str = 'cog_vlm'):
     model_server_config = get_server_config(model_name)
@@ -28,5 +21,4 @@ def create_cog_app(model_name: str = 'cog_vlm'):
     uvicorn.run(my_app, host=model_server_config['host'], port=model_server_config['port'])
 
 if __name__ == '__main__':
-    #create_med_app()
     create_cog_app()
